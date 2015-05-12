4d7f7b9bd7d9f807a2fb886077eb6ed1676e3cb5
SeevenDev <seeven.dev@gmail.com>
2015-05-12 22:40:02 +0200

    Premier commit

---
+++ www/wip/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Work in Progress</title>
+</head>
+<body>
+	<h1>Work in Progress</h1>
+	<p>Répertoire de travail pour le site.</p>
+</body>
+</html>
