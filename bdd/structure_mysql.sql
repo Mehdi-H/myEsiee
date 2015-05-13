f2bb538dad0f1a8e4f7917275a5896db56f42315
SeevenDev <seeven.dev@gmail.com>
2015-05-13 21:20:19 +0200

    Ajout du code SQL utilisé pour créer les tables "salle" et "prof" de la BDD perso.

---
+++ bdd/structure_mysql.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS salle;
+CREATE TABLE salle (
+	nom_salle			varchar(8) PRIMARY KEY,
+	resourceID_salle	int(11) unsigned NOT NULL UNIQUE,
+	projecteur 			tinyint(1) unsigned NOT NULL DEFAULT 0,
+	tableau 			tinyint(2) unsigned NOT NULL DEFAULT 0,
+	taille 				int(11) unsigned NOT NULL DEFAULT 0,
+	imprimante			tinyint(1) unsigned NOT NULL DEFAULT 0
+);
+
+DROP TABLE IF EXISTS prof;
+CREATE TABLE prof (
+	nom_prof			varchar(8) PRIMARY KEY,
+	resourceID_prof		int(11) unsigned NOT NULL UNIQUE,
+	bureau 				tinyint(1) unsigned NOT NULL DEF	AULT 0,
+	email 				tinyint(2) unsigned NOT NULL DEFAULT 0,
+);
+
