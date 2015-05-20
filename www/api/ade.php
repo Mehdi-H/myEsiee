72e354f15fa65a9c92af49f3a594bdd7f317b39c
SeevenDev <seeven.dev@gmail.com>
2015-05-20 18:14:17 +0200

    [ADE] Ajout de la fonction "rechSalle" à appeler pour lister les salles selon certains critères.

--- www/api/ade.php
+++ www/api/ade.php
@@ -1,45 +1,17 @@
 <?php
-		if (isset($_GET['resource'])
-			&& isset($_GET['date'])
-			&& isset($_GET['heure']))
-		{
-			$resource = $_GET['resource'];
-			$date = $_GET['date'];
-			$heure = $_GET['heure'];
+		if (isset($_GET['func'])) {
+			$function = $_GET['func'];
 		}
-		else { ?>
-
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="fr" xml:lang="fr">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-
-	<title>Script ADE</title>
-	<link href="http://static.alwaysdata.com/v3/images/favicon.ico" rel="shortcut icon" />
-
-	<!-- CSS -->
-	<link href="css/style.css" rel="stylesheet" type="text/css">
-</head>
-
-<body>
-	<p>Spécifiez la ressource (genre 659 pour la salle 5201V...), la date (format jj/mm/aaaa) et l'heure à vérifier (format hh:mm)</p>
-	<p>Exemple : <pre>https://mvx2.esiee.fr/api/ade.php?resource=659&date=05/05/2015&heure=09:00</pre> (la salle est occupée)</p>
-	<p>Exemple : <pre>https://mvx2.esiee.fr/api/ade.php?resource=659&date=05/05/2015&heure=13:30</pre> (la salle est libre)</p>
-</body>
-</html>
-
-		<?
+		else {
 			exit;
 		}
 
 		/////////////////////////////////////////////////////////////////////////////////////////
-		/// FONCTIONS
+		/// FONCTIONS PERSOS
 		/////////////////////////////////////////////////////////////////////////////////////////
 
 		function httpGet($url)
 		{
-			echo("Requête : " . $url);
-
 			// Création de la requête :
 			$ch = curl_init();
 
@@ -54,12 +26,19 @@
 
 			$out = curl_exec($ch); // Exécution
 			curl_close($ch); // Fermeture
+
+			//echo('url : '.$url.'<pre>'.htmlspecialchars($out).'</pre>');
+
 			return new SimpleXMLElement($out);
 		}
 
 		/////////////////////////////////////////////////////////////////////////////////////////
-		/// Connexion à ADE
+		/// Préparation (connexion à ADE, choix du projet ADE)
 		/////////////////////////////////////////////////////////////////////////////////////////
+
+		// === VARIABLES ===
+
+		$projetADE = 4;
 
 		// === Se connecter à une session ===
 
@@ -67,7 +46,6 @@
 			."?function=connect"
 			."&login=lecteur1&password=");
 
-		//$req_xml = new SimpleXMLElement($req);
 		$sessionId = $req['id'];
 
 		// === Se placer sur le projet 4 (EDT 2014-2015) ===
@@ -75,71 +53,148 @@
 		httpGet("https://planif.esiee.fr:8443/jsp/webapi"
 			."?sessionId=".$sessionId
 			."&function=setProject"
-			."&projectId=4");
+			."&projectId=".$projetADE);
 
 		/////////////////////////////////////////////////////////////////////////////////////////
-		/// Afficher les horaires où la salle 5201V est occupée
-		/// - ID de ressource de la salle 5201V : 659
+		/// FONCTION : Recherche de salles
 		/////////////////////////////////////////////////////////////////////////////////////////
 
-		// === Récupérer toutes les activités utilisant la salle ===
-
-		$req = httpGet("https://planif.esiee.fr:8443/jsp/webapi"
-			."?sessionId=".$sessionId
-			."&function=getEvents"
-			."&detail=0"
-			."&resources=659" // salle 5201V
-			."&date=".$date
-		);
-
-		// === Afficher tous les horaires où elle est utilisée ===
-
-		$horaires = array();
-
-		foreach ($req->xpath("//event") as $event)
+		if (strcmp($function, "rechSalle") == 0)
 		{
-			$start = "".$event['startHour'];
-			$end = "".$event['endHour'];
+			// === PARAMETRES DE LA RECHERCHE ===
 
-			// Ajouter les horaires de l'event à cette date :
-			$horaires[] = array($start, $end);
-		}
-
-		echo("<pre>");
-		print_r($horaires);
-		echo("</pre>");
-
-		// === Vérifier si la salle est libre ===
-
-		$libre = true;
-		$until = "";
-
-		foreach ($horaires as $event)
-		{
-			$start = $event[0];
-			$end = $event[1];
-			$h_demande = $heure;
-
-			// Date to integer :
-			str_replace(":", "", $start);
-			str_replace(":", "", $end);
-			str_replace(":", "", $h_demande);
-
-			// Comparer heure demandée :
-			if ($start <= $h_demande && $h_demande < $end) {
-				// Salle occupée :
-				$libre = false;
-				$until = $event[1];
+			// Type de salle :
+			if (isset($_GET['ty'])) {
+				$type = $_GET['ty'];
 			}
-		}
 
-		// Verdict :
+			// "misc" = "PTIS" (Projecteur [0,1], Tableau [0,1,2,3], Imprimante [0,1], Size [entier])
+			// Tableau : [0:aucun, 1:blanc, 2:noir, 3:deux]
+			if (isset($_GET['misc'])) {
+				$projecteur = substr($_GET['misc'], 0, 1);
+				$tableau = substr($_GET['misc'], 1, 1);
+				$imprimante = substr($_GET['misc'], 2, 1);
+				$taille = substr($_GET['misc'], 3);
 
-		if ($libre) {
-			echo("<p>La salle est libre !</p>");
-		}
-		else {
-			echo("<p>La salle est occupée jusqu'à ".$until."</p>");
+				$sql_misc = " AND taille=".$taille
+					." AND projecteur=".$projecteur
+					." AND tableau=".$tableau
+					." AND imprimante=".$imprimante;
+			} else {
+				$sql_misc = "";
+			}
+
+			// === Lister les salles correspondant aux critères (ty et misc) dans la BDD ===
+
+			// Connexion à la BDD :
+			$db = mysql_connect('localhost', 'root', 'MyMVX2');
+			mysql_select_db('eroom', $db);
+
+			// Requête SQL :
+			$sql = "SELECT * FROM salle "
+				."WHERE type_salle LIKE '".$type."'".$sql_misc;
+
+			echo($sql.'<br/>');
+
+			// Exécution :
+			$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_errno());
+
+			// Stockage :
+			$salles = array();
+			while ($data = mysql_fetch_assoc($req))
+			{
+				$salles[$data[nom_salle]] = array(
+					"id" => $data[resourceID_salle],
+					"type" => $data[type_salle],
+					"taille" => $data[taille],
+					"projecteur" => $data[projecteur],
+					"tableau" => $data[tableau],
+					"imprimante" => $data[imprimante],
+					"dispo" => -1
+				);
+			}
+
+			mysql_close();
+
+			// === Date et heure actuelles ===
+
+			$now_date = date("d/m/Y"); // format "20/05/2015"
+			$now_date_ADE = date("m/d/Y"); // format "05/20/2015" pour les requêtes ADE
+			$now_time = date("H:i"); // format "15:07"
+
+			echo($now_date.' - '.$now_time.'<br/>');
+
+			// === Récupérer la disponibilité de chaque salle ===
+
+			foreach ($salles as $nom_salle => $salle)
+			{
+				// --- Requête ADE ---
+
+				$req = httpGet("https://planif.esiee.fr:8443/jsp/webapi"
+					."?sessionId=".$sessionId
+					."&function=getEvents"
+					."&detail=0"
+					."&resources=".$salle[id]
+					."&date=".$now_date_ADE
+				);
+
+				// --- Lecture des horaires ---
+
+				$horaires = array();
+				foreach ($req->xpath("//event") as $event)
+				{
+					$start = "".$event['startHour'];
+					$end = "".$event['endHour'];
+
+					// Ajouter les horaires de l'event à cette date :
+					$horaires[] = array($start, $end);
+				}
+				// echo('<pre>'); print_r($horaires); echo('</pre>');
+
+				// --- Calcul du temps libre restant ---
+				// Pas libre, libre jusqu'à telle heure, libre toute la journée
+
+				$libre = true;
+				$pendant = -1;
+
+				foreach ($horaires as $cours)
+				{
+					$debut = strtotime($cours[0]);
+					$fin = strtotime($cours[1]);
+
+					if ($fin < time())
+					{
+						// Si l'heure de fin est dépassée : on ne s'occupe pas de cette horaire.
+						continue;
+					}
+					elseif (time() < $debut && $libre)
+					{
+						// Si on est avant l'heure de début : combien de temps on est libre ?
+						$pendant = $debut - time();
+					}
+					elseif ($debut <= time())
+					{
+						// Si on est après le début d'un cours (et avant la fin grâce au premier IF) : pas libre
+						$libre = false;
+					}
+				}
+
+				if ($libre && $pendant > 0) {
+					echo("La salle ".$nom_salle." est libre jusqu'a ".$cours[1].", cad pendant ".$pendant." minutes.<br>");
+					$salles[$nom_salle][dispo] = $pendant;
+				} elseif ($libre) {
+					echo("La salle ".$nom_salle." est libre jusqu'a ce soir.<br>");
+					$salles[$nom_salle][dispo] = "full";
+				} else {
+					echo("La salle ".$nom_salle." n'est pas libre.<br>");
+					$salles[$nom_salle][dispo] = 0;
+				}
+
+			}
+			echo('<pre>'); print_r($salles); echo('</pre>');
+
+			echo('<pre>'.json_encode($salles).'</pre>');
+
 		}
 
 		/////////////////////////////////////////////////////////////////////////////////////////
