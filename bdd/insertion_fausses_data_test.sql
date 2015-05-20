2388fc80b13adb139c702aef04d14070ddf73b00
SeevenDev <seeven.dev@gmail.com>
2015-05-20 14:17:57 +0200

    [BDD] Modification du code de structure de la table "salle". Création d'un code d'insertion de fausses données pour des tests.

---
+++ bdd/insertion_fausses_data_test.sql
@@ -0,0 +1,8 @@
+INSERT INTO salle
+	(nom_salle, resourceID_salle, type_salle, taille, projecteur, tableau, imprimante)
+VALUES
+	("5004", 789, "it", 24, 0, 2, 0),
+	("5006", 790, "it", 24, 0, 2, 0),
+	("5008V+", 786, "it", 26, 1, 2, 0),
+	("5201V", 659, "it", 24, 1, 2, 0),
+	("2101", 183, "vid", 12, 1, 2, 0);
--- bdd/structure_mysql.sql
+++ bdd/structure_mysql.sql
@@ -2,10 +2,11 @@ DROP TABLE IF EXISTS salle;
 CREATE TABLE salle (
 	nom_salle			varchar(8) PRIMARY KEY,
 	resourceID_salle	int(11) unsigned NOT NULL UNIQUE,
-	projecteur 			tinyint(1) unsigned NOT NULL DEFAULT 0,
-	tableau 			tinyint(2) unsigned NOT NULL DEFAULT 0,
+	type_salle			varchar(30),
 	taille 				int(11) unsigned NOT NULL DEFAULT 0,
-	imprimante			tinyint(1) unsigned NOT NULL DEFAULT 0
+	projecteur 			tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT '0:non, 1:oui',
+	tableau 			tinyint(2) unsigned NOT NULL DEFAULT 0 COMMENT '0:aucun, 1:blanc, 2:noir, 3:deux',
+	imprimante			tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT '0:non, 1:oui'
 );
 
 DROP TABLE IF EXISTS prof;
