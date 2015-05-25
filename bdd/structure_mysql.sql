7473df20d6303595964b70a31894a0de0862cde4
Mehdi-H <mehdi.houacine@edu.esiee.fr>
2015-05-25 18:27:31 +0200

    [ADE] Modification du script de récupération des salles pour placer un id aléatoire aux resourcesID des salles. [bdd] Ajout des requetes générées sous formes textuelles, à tester.

--- bdd/structure_mysql.sql
+++ bdd/structure_mysql.sql
@@ -1,3 +1,6 @@
+/**
+ * Requetes pour créer les tables sur le serveur.
+ */
 DROP TABLE IF EXISTS salle;
 CREATE TABLE salle (
 	nom					varchar(15) PRIMARY KEY,
@@ -17,3 +20,16 @@ CREATE TABLE prof (
 	email 				varchar(30) DEFAULT NULL
 );
 
+
+/**
+ * Requete pour ajout dans ma bdd locale pour tests.
+ */
+CREATE TABLE salle (
+      nom varchar(8) PRIMARY KEY,
+      resourceID int(20) unsigned NOT NULL UNIQUE,
+      type varchar(30),
+      taille int(11) unsigned NOT NULL DEFAULT 0,
+      projecteur tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT '0:non, 1:oui',
+      tableau tinyint(2) unsigned NOT NULL DEFAULT 0 COMMENT '0:aucun, 1:blanc, 2:noir, 3:deux',
+      imprimante tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT '0:non, 1:oui'
+    ) ENGINE=InnoDB
