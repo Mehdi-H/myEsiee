063ac943f1f34891fbb5b43be5a584b218917395
Mehdi-H <mehdi.houacine@edu.esiee.fr>
2015-05-25 14:45:44 +0200

    [ADE] Scripts python ajoutés à csv_to_rooms.py pour extraire les salles et leurs infos dans une structure en vue de l'importer en BDD. La structure comprendles infos des salles de travail (pas les bureaux) de l'excel de la planification, l'excel de Johann avec tableau/projo/imprimantes, et les noms de salles correspondent aux noms sur ADE.

---
+++ bdd/salle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: mehdi
+# @Date:   2015-05-25 12:53:52
+# @Last Modified by:   mehdi
+# @Last Modified time: 2015-05-25 14:34:48
+
+import pprint as pp
+
+# /**
+#  * Une salle de l'esiee
+#  */
+class Salle:
+    
+    def __init__(self, nom, resourceID, fonction, taille, projecteur, tableau, imprimante):
+        self.nom = nom  # string
+        self.resourceID = resourceID
+        self.fonction = fonction  # string
+        self.taille = taille  # int
+        self.projecteur = projecteur  # int '0:non, 1:oui'
+        self.tableau = tableau  # int '0:aucun, 1:blanc, 2:noir, 3:deux'
+        self.imprimante = imprimante  # int '0:non, 1:oui'
+    
+    def display_salle(self):
+        pp.pprint(self.__dict__)
+    
+    def set_board(self,b):
+        self.tableau = b
+    
+    def set_projector(self,p):
+        self.projecteur = p
+    
+    def set_printer(self,i):
+        self.imprimante = i
+        
+    def set_bpp(self,b,p,i):
+        self.set_board(b)
+        self.set_projector(p)
+        self.set_printer(i)
